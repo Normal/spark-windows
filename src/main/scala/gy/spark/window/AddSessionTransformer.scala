cdd25e6401c5c212ff013521435027e0213c8b6e
George Yarish <george.yarish@macys.com>
2018-07-28 02:04:42 +0300

    implemented session calculation

--- src/main/scala/gy/spark/window/AddSessionTransformer.scala
+++ src/main/scala/gy/spark/window/AddSessionTransformer.scala
@@ -1,29 +1,49 @@
 package gy.spark.window
 
+import java.util.UUID
+
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class AddSessionTransformer(spark: SparkSession) {
 
-  def transform(df: DataFrame): DataFrame = {
+  def transform(eventsDF: DataFrame): DataFrame = {
 
-    import org.apache.spark.sql.expressions.Window
     import org.apache.spark.sql.functions._
 
-//    val windowSpec = Window.partitionBy(col("category")).orderBy(col("eventTime").asc)
-////    val sessionDuration = "5 minutes"
-//
-//    df.select(
-//      col("userId"),
-//      col("eventTime"),
-//      col("category"),
-//      first("eventTime").over(windowSpec).as("firstEventTime"),
-//      lag("eventTime", 1).over(windowSpec).as("prevEventTime")
-//    )
+    eventsDF.createOrReplaceTempView("events")
 
-    df
-      .groupBy(col("category"), window(col("eventTime"), "5 minutes"))
-      .pivot("userId")
-      .agg(rand() as "session_id")
-      .select("window.start", "window.end", "category", "session_id", "userId")
+    val randUid = udf(() => UUID.randomUUID().toString.substring(0, 5))
+
+    val sessionDF = eventsDF
+      .groupBy(col("category"), col("userId"), window(col("eventTime"), "5 minutes"))
+      .agg(randUid() as "sessionId")
+      .select(
+        col("category"),
+        col("userId"),
+        col("sessionId"),
+        col("window.start") as "sessionStartTime",
+        col("window.end") as "sessionEndTime"
+      )
+    sessionDF.createOrReplaceTempView("sessions")
+
+    val fields = List(
+      "events.category",
+      "events.product",
+      "events.userId",
+      "events.eventTime",
+      "events.eventType",
+      "sessions.sessionId",
+      "sessions.sessionStartTime",
+      "sessions.sessionEndTime"
+    ).mkString(",")
+
+    val joinClauses = List(
+      "events.userId == sessions.userId",
+      "events.category == sessions.category",
+      "events.eventTime < sessions.sessionEndTime",
+      "events.eventTime >= sessions.sessionStartTime"
+    ).mkString(" AND ")
+
+    spark.sql(s"SELECT $fields FROM events LEFT JOIN sessions ON $joinClauses")
   }
 }
