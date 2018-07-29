c49e73ed1bd17c0d1171bd8d2756f8e1d99d14fe
YH04236 <george.yarish@macys.com>
2018-07-29 03:53:27 -0700

    implemented product ranking

--- src/main/scala/gy/spark/window/ProductRankingTransformer.scala
+++ src/main/scala/gy/spark/window/ProductRankingTransformer.scala
@@ -1,8 +1,5 @@
 package gy.spark.window
 
-import java.util.UUID
-
-import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -11,38 +8,55 @@ class ProductRankingTransformer(spark: SparkSession) {
   def transform(eventsDF: DataFrame): DataFrame = {
 
     import org.apache.spark.sql.functions._
-    import spark.implicits._
-
-    val randUid = udf(() => UUID.randomUUID().toString.substring(0, 5))
 
     val userWindow = Window.partitionBy("userId").orderBy("eventTime")
-    val userProductWindow = Window.partitionBy("userId", "product").orderBy("eventTime")
-    val sessionWindow = Window.partitionBy("category", "userId", "product", "sessionId").orderBy("eventTime")
+    val sessionWindow = Window.partitionBy("userId").orderBy("sessionStart")
 
+    /*
+        Populates initial dataframe with session data.
+        Session lasts until the user is looking at particular product.
+        When particular user switches to another product the new session starts.
+
+        |category|product|userId|eventTime|eventType|sessionStart|sessionEnd|
+     */
     val sessionDF = eventsDF
       .withColumn("firstTS", when(col("product") === lag("product", 1).over(userWindow), lit(null)).otherwise(unix_timestamp(col("eventTime"))))
       .orderBy(col("category"), col("userId"), col("eventTime"), col("firstTS").desc)
       .withColumn("sessionStart", last(col("firstTS"), ignoreNulls = true).over(userWindow.rowsBetween(Window.unboundedPreceding, 0)))
-//      .withColumn("sessionStart", last())
+      .withColumn("sessionEnd", unix_timestamp(max(col("eventTime")).over(sessionWindow)))
+      .drop("firstTS")
 
-//      .withColumn("sessionId", when(col("product") === col("prevProduct"), sessionIterator(lit(0))).otherwise(sessionIterator(lit(1))))
-//      .drop("prevProduct", "isNewSession")
-//    sessionDF.show()
+    /*
+        Grouped by session, calculated duration for each session.
 
-    sessionDF
-//      .groupBy("sessionId")
-//      .agg((unix_timestamp(max(col("eventTime"))) - unix_timestamp(min(col("eventTime")))).as("sessionDuration"))
+        |category|product|userId|sessionDurationSec|
+      */
+    val durationDF = sessionDF
+      .withColumn("sessionDurationSec", col("sessionEnd") - col("sessionStart"))
+      .select("category", "product", "userId", "sessionDurationSec")
+      .distinct()
+      .orderBy("category", "userId", "eventTime")
 
+    /*
+        |category|product|totalTimeSpent|
+     */
+    val totalDurationDF = durationDF
+      .groupBy("category", "product")
+      .agg(sum(col("sessionDurationSec")).as("totalTimeSpent"))
+      .orderBy(col("category"), col("totalTimeSpent").desc)
 
-    //      .withColumn("sessionDurationSec", unix_timestamp(col("sessionEndTime")) - unix_timestamp(col("sessionStartTime")))
-    //      .drop("sessionStartTime", "sessionEndTime")
-    //      .orderBy("category", "userId", "eventTime")
+    /*
+        |category|product|totalTimeSpent|rank|
+     */
+    val categoryWindow = Window.partitionBy("category").orderBy(col("totalTimeSpent").desc)
+    val rankedDF = totalDurationDF
+      .withColumn("rank", rank.over(categoryWindow))
 
-    //    val sumDurationDF = sessionDF
-//      .select("category", "product", "userId", "sessionId", "sessionDurationSec")
-//      .distinct()
-//
-//    sumDurationDF
+    rankedDF
+        .filter(col("rank") <= 2)
+        .select("category", "product")
+        .orderBy("category", "rank")
+
   }
 
 }
